80446c30a7601e92aaa4b46bf11ff4b4ce75ad9e
Zach <thezachrose@gmail.com>
2018-04-05 10:14:27 -0500

    Create WordMeaningNode.java

---
+++ WordMeaningNode.java
@@ -0,0 +1,11 @@
+public class WordMeaningNode 
+{
+    WordMeaning data;
+    WordMeaningNode next = null;
+
+    WordMeaningNode(WordMeaning d)
+    {
+        data = d;
+        next = null;
+    }
+}
