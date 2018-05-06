a2f262ac327855bc319c260bbee0b2cf2720a3f2
Zach <thezachrose@gmail.com>
2018-05-06 07:26:52 -0500

    Update WordMeaning.java

--- WordMeaning.java
+++ WordMeaning.java
@@ -1,19 +1,23 @@
 public class WordMeaning 
 {
+   //initializing words and their corresponding meanings
    private String name;
    private String meaning;
    
+   //constructor method to establish a word and definition that will be added to the list
    public WordMeaning(String n, String m)
    {
        name = n;
        meaning = " - " + m;
    }
    
+   //accessor method to fetch word
    public String getWord()
    {
        return name;
    }
    
+   //accessor method to fetch meaning
    public String getMeaning()
    {
        return meaning;
